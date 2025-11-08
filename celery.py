0aba0df82dc2af5072fc03b803f637b327d1240e
UFUOMA OGEDEGBE <ufuogbe@gmail.com>
2025-11-08 03:59:28 +0000

    celery.py deleted

--- celery.py
+++
@@ -1,9 +0,0 @@
-# project_root/celery.py
-import os
-from celery import Celery
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project.settings")
-
-app = Celery("your_project")
-app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
